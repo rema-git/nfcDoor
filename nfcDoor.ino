9e6c6ac7c3eb1e0979a21b284eb0dfe0dd5718b4
Reinhardt A.W. Maier <rema@zaehlwerk.net>
2015-06-05 15:18:10 +0200

    initial commit: nfcDoor a project of 2013

---
+++ nfcDoor.ino
@@ -0,0 +1,266 @@
+/*
+ *
+ *  NFC based door access system
+ *
+ *  Reinhardt A.W. Maier, 2013
+ *  rema@zaehlwerk.net
+ *
+ */
+#define IRQ      (2)
+#define RESET    (8)
+#define DOORPIN  (3)
+#define LEDPIN   (10)
+#define DOOROPENTIME (10000) // in ms
+
+#include <Wire.h>
+#include <Arduino.h>
+#include <Adafruit_NFCShield_I2C.h>
+
+struct keylist
+{
+  char name[30];
+  uint8_t uid[4];
+  uint8_t keyA[6];
+  uint8_t passwd[16];
+};
+
+static keylist building01[] = {
+  {{"Tester A"}
+   ,{0xA1, 0xA1, 0xA1, 0xA1}
+   ,{0x11, 0xFF, 0xFF, 0xF8, 0xFF, 0xFF}
+   ,{'t', 'e', 's', 't', '1', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+};
+
+struct led_t {
+  short cycle;                // number of runs
+  unsigned short brightness;  // 0 = off pwm, 255 = full time on
+  short fadeAmount;           // speed
+} 
+led = {1, 0, 1};
+
+Adafruit_NFCShield_I2C nfc(IRQ, RESET);
+
+boolean debug = false;
+static int keylistlength = sizeof(building01) / (sizeof building01[0]);
+
+void setup() {
+  // pin modes
+  pinMode(DOORPIN, OUTPUT);
+  pinMode(LEDPIN, OUTPUT);
+  digitalWrite(DOORPIN, LOW);
+
+  // serial connection
+  Serial.begin(115200);
+
+  // nfc chip connection
+  nfc.begin();
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  nfc.SAMConfig();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board. Halt!");
+    while (1); // halt
+  }
+  Serial.print("Found chip PN5"); 
+  Serial.println((versiondata>>24) & 0xFF, HEX); 
+  Serial.print("Firmware ver. "); 
+  Serial.print((versiondata>>16) & 0xFF, DEC); 
+  Serial.print('.'); 
+  Serial.println((versiondata>>8) & 0xFF, DEC);
+
+  // print status
+  ledStatus(6);
+  Serial.println("Debug mode: OFF (type 'd' to enable)");
+  Serial.println("Boot sequence completed. Ready to read a tag.");
+  Serial.println("");
+}
+
+void loop() {
+  uint8_t success = 0;
+  uint8_t uid[] = {0, 0, 0, 0, 0, 0, 0};
+  uint8_t keyA[] = {0, 0, 0, 0, 0, 0, 0}; 
+  uint8_t uidLength = 0;
+  uint8_t passwd[16];
+  char messsage;
+
+  // wait until a tag enters rf field
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+  // analyse tag
+  if(success){
+    if(uidLength == 4) {
+      int index = -1;
+      index = getIndexByUid(uid);
+      if(index >= 0) {
+        getKeyAByIndex(index,keyA);
+        success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keyA);
+        success = success + nfc.mifareclassic_ReadDataBlock(4, passwd);
+        if(success == 2) {
+          success = checkPasswdByIndex(index, passwd);
+          if(success) {
+            debugMeassage("OK:    door open!");
+            openDoor();
+          }
+          else {
+            ledStatus(2);
+            debugMeassage("ERROR: tag contains wrong password.");
+          }
+        }
+        else {
+          ledStatus(3);
+          debugMeassage("ERROR: authentification request failed.");
+        }
+      }
+      else {
+        ledStatus(4);
+        debugMeassage("ERROR: tag is not in key list.");
+      }
+    }
+    else {
+      ledStatus(5);
+      debugMeassage("ERROR: tag detection failed or wrong card typ.");
+    }
+    if(debug == true){
+      Serial.println();
+      Serial.print("UID Length: ");
+      Serial.print(uidLength, DEC);
+      Serial.println(" bytes");
+      Serial.print("UID Value: ");
+      nfc.PrintHex(uid, uidLength);
+      Serial.println();
+      Serial.println("...waiting...");
+      Serial.println();
+    }
+    // anti latch-up
+    delay(1000);
+  }
+}   
+
+int8_t getIndexByUid(const byte * uid) {
+  for (int index = 0; index < keylistlength; index++) {
+    int correlation = 0;
+    for (int n = 0; n < 4; n++) {
+      if (uid[n] == building01[index].uid[n])
+        correlation++;
+      if(correlation == 4){
+        debugMeassage("OK:    tag ID is known, name:");
+        debugMeassage(building01[index].name);
+        return index;
+      }
+    }
+  }
+  return -1;
+}
+
+void getKeyAByIndex(int index, uint8_t * keyA) {
+  memcpy (keyA, building01[index].keyA, 7);
+}
+
+uint8_t checkPasswdByIndex(int index, uint8_t * passwd) {
+  for (int n = 0; n < 16; n++) {
+    if (passwd[n] != building01[index].passwd[n])
+      return 0;
+  }
+  debugMeassage("OK:    password check passed.");
+  return 1;
+}
+
+void openDoor() {
+  ledStatus(1);
+  digitalWrite(DOORPIN, HIGH);
+  delay(DOOROPENTIME);     // duration of door opener in ms
+  digitalWrite(DOORPIN, LOW);
+  ledStatus(0);
+}
+
+void ledStatus(int led_code) {
+  switch (led_code) {
+  case 0:    // off
+    digitalWrite(LEDPIN, LOW);
+    break;
+  case 1:    // on
+    digitalWrite(LEDPIN, HIGH);
+    break;
+  case 2:    // error_passwd
+    for(unsigned short m = 0; m <= 4; m++){
+      for(unsigned short n = 0; n <= 3; n++){
+        digitalWrite(LEDPIN, HIGH);
+        delay(50);
+        digitalWrite(LEDPIN, LOW);
+        delay(25);
+      }
+      delay(250);
+    }
+    break;
+  case 3:    // error_auth
+    for(unsigned short m = 0; m <= 3; m++){
+      for(unsigned short n = 0; n <= 3; n++){
+        digitalWrite(LEDPIN, HIGH);
+        delay(50);
+        digitalWrite(LEDPIN, LOW);
+        delay(25);
+      }
+      delay(250);
+    }
+    break;
+  case 4:    // error_unknown_tag
+    for(unsigned short m = 0; m <= 1; m++){
+      for(unsigned short n = 0; n <= 3; n++){
+        digitalWrite(LEDPIN, HIGH);
+        delay(50);
+        digitalWrite(LEDPIN, LOW);
+        delay(25);
+      }
+      delay(250);
+    }
+    break;
+  case 5:    // error_tag_typ
+    for(unsigned short m = 0; m <= 2; m++){
+      for(unsigned short n = 0; n <= 3; n++){
+        digitalWrite(LEDPIN, HIGH);
+        delay(50);
+        digitalWrite(LEDPIN, LOW);
+        delay(25);
+      }
+      delay(250);
+    }
+    break;
+  case 6:    // bootup
+    for(unsigned short n = 0; n <= 4; n++){
+      digitalWrite(LEDPIN, HIGH);
+      delay(120);
+      digitalWrite(LEDPIN, LOW);
+      delay(120);
+    }
+    break;
+  case 9:   // fading
+    if(led.cycle <= 0){
+      analogWrite(LEDPIN, led.brightness);
+      led.brightness = led.brightness + led.fadeAmount;
+      if(led.brightness <= 0 || led.brightness >= 64)
+        led.fadeAmount = -led.fadeAmount;
+      if (led.fadeAmount < 0)
+        led.cycle = 1000;
+      if(led.fadeAmount > 0)
+        led.cycle = 3000;
+    }
+    led.cycle--;
+    break;
+  }
+}
+
+void debugMeassage(const char *message){
+  if (Serial.available() > 0) {
+    int incomingByte = Serial.read();
+    if (incomingByte == 'd') {
+      debug = true;
+      Serial.println("Debug mode: ON (type any key to disable)");      
+    }
+    else {
+      debug = false;
+      Serial.println("Debug mode: OFF (type 'd' to enable)");
+    }
+  }
+  if(debug == true){
+    Serial.println(message);
+  }
+}
---
+++ tools/tagWriter/tagWriter.ino
@@ -0,0 +1,180 @@
+/*
+ *
+ *  NFC tag writer
+ *
+ *  Reinhardt A.W. Maier, 2013
+ *  rema@zaehlwerk.net
+ *
+ */
+
+#include <Wire.h>
+#include <Adafruit_NFCShield_I2C.h>
+
+#define IRQ   (2)
+#define RESET (3)
+
+Adafruit_NFCShield_I2C nfc(IRQ, RESET);
+
+
+// *********************************
+// SET DATA HERE TO WRITE TO THE TAG
+// *********************************
+
+uint8_t passwd[16] = { 
+  's', 'e', 'c', 'r', 'e', 't', '1', 0, 0, 0, 0, 0, 0, 0, 0, 0};
+uint8_t keyA[16] = { 
+  0x1A, 0xFE, 0xFA, 0xF8, 0xFF, 0xFF,   // new KeyA
+  0xFF, 0x07, 0x80, 0xFF,               // Access Bits, never change!!!
+  0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF }; // KeyB, has no rights
+
+// *********************************
+
+
+void setup(void) {
+  Serial.begin(115200);
+
+  nfc.begin();
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); 
+  Serial.println((versiondata>>24) & 0xFF, HEX); 
+  Serial.print("Firmware ver. "); 
+  Serial.print((versiondata>>16) & 0xFF, DEC); 
+  Serial.print('.'); 
+  Serial.println((versiondata>>8) & 0xFF, DEC);
+
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+
+  Serial.println("Waiting for an ISO14443A Card ...");
+}
+
+void loop(void) {
+  uint8_t success;
+  uint8_t uid[] = {0, 0, 0, 0, 0, 0, 0};
+  uint8_t uidLength;
+  uint8_t data[16];
+
+  // Wait for an ISO14443A type cards (Mifare) -> blocking!
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+  if (success) {
+    // display some basic information about the card
+    Serial.println("Found an ISO14443A card");
+    Serial.print("  UID Length: ");
+    Serial.print(uidLength, DEC);
+    Serial.println(" bytes");
+    Serial.print("  UID Value: ");
+    nfc.PrintHex(uid, uidLength);
+    Serial.println("");
+
+    if (uidLength == 4)
+    {
+      Serial.println("Seems to be a Mifare Classic card (4 byte UID)");
+
+
+      //
+      // AUTHENTICATE
+      //
+      Serial.println("Trying to authenticate block 4 with factory default KeyA");
+      uint8_t keya_factory_default[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+      // block 4 (the first block of sector 1)
+      success = nfc.mifareclassic_AuthenticateBlock(uid, uidLength, 4, 0, keya_factory_default);
+        
+        
+      //
+      // WRITE PASSWD
+      //
+      if (success)
+      {
+        Serial.println("Sector 1 (Blocks 4..7) has been authenticated");
+
+        success = nfc.mifareclassic_WriteDataBlock (4, passwd);
+        if (success)
+        {
+          // Data seems to have been written
+          Serial.println("");
+          Serial.println("Writing Block 4 succeded!");
+        }
+        else
+        {
+          Serial.println("ERROR: unable to write new passwd.");
+          halt();
+        }
+        
+        // Try to read the contents of block 4
+        success = nfc.mifareclassic_ReadDataBlock(4, data);
+        if (success)
+        {
+          Serial.println("Reading Block 4:");
+          nfc.PrintHexChar(data, 16);
+          Serial.println("");
+        }
+        else
+        {
+          Serial.println("ERROR: unable to read the requested block.");
+          halt();
+        }
+      }
+
+
+      //
+      // WRITE NEW KEYA
+      //
+      if (success)
+      {
+        success = nfc.mifareclassic_WriteDataBlock (7, keyA);
+        if (success)
+        {
+          // Data seems to have been written
+          Serial.println("");
+          Serial.println("Writing Block 7 succeded! New keyA written.");
+        }
+        else
+        {
+          Serial.println("ERROR: unable to write the new keyA.");
+          halt();
+        }
+        
+        // Try to read the contents of block 7
+        success = nfc.mifareclassic_ReadDataBlock(7, data);
+        if (success)
+        {
+          Serial.println("Reading Block 7 (keyA is blanked out):");
+          nfc.PrintHexChar(data, 16);
+          Serial.println("");
+        }
+        else
+        {
+          Serial.println("ERROR: unable to read the requested block.");
+          halt();
+        }
+      }
+      else
+      {
+        Serial.println("RERROR: authentication failed.");
+        halt();
+      }
+    }
+    else
+    {
+      Serial.println("ERROR: wrong card typ.");
+      halt();
+    }
+  
+}
+  halt();
+}
+
+void halt(void) {
+  Serial.println("");
+  Serial.println("System halted!!! Please reboot!");
+  Serial.println("");
+  while(1);
+}
